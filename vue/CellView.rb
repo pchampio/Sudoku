ffae16cd6c50b7b0224c4d73339fe33978d3d1c5
mariusmaxetselina <maxetselina@gmail.com>
2017-01-31 14:31:06 +0100

    modification de CellView

--- vue/CellView.rb
+++ vue/CellView.rb
@@ -1,9 +1,16 @@
 require 'gtk3'
+require_relative '../class/cell_class.rb'
 
 class CellView < Gtk::Button
-	@cell
+	
 
-	def Initialize(value)
-		super(value)
+	def initialize(cell)
+		@cell=cell
+		if cell.value==0
+			super(:label=>" ")
+		else
+			super(:label=>cell.value.to_s)
+		end
+		
 	end
 end
