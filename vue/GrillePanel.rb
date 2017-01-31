9cf91278a3a2e0d71b8c715dabe54b96e7227cb3
mariusmaxetselina <maxetselina@gmail.com>
2017-01-31 16:37:59 +0100

    modification de CellView et GrillePanel

--- vue/GrillePanel.rb
+++ vue/GrillePanel.rb
@@ -7,13 +7,25 @@ class GrillePanel
 
 	def initialize(board)
 		@board=board
-		@boardView=Gtk::Table.new(9,9,true)
-		0.upto(8){|y|
-			0.upto(8){|x|
-				cell=CellView.new(@board.cellAt(x,y))
-				@boardView.attach(cell.button,x,x+1,y,y+1)
+		@boardView=Gtk::Table.new(3,3,true)
+
+		@boxView=Array.new(3){Array.new(3)}
+
+		0.upto(2){|y|
+			0.upto(2){|x|
+				@boxView[x][y]=Gtk::Table.new(3,3,true)
+				
+				0.upto(2){|i|
+					0.upto(2){|j|
+						cell=CellView.new(@board.cellAt(x*3+i,y*3+j))
+						@boxView[x][y].attach(cell.button,i,i+1,j,j+1)
+					}
+				}
+				@boardView.attach(@boxView[x][y],x,x+1,y,y+1,nil,nil,2,2)
+				@boxView[x][y].show
 			}
 		}
+		
 		@boardView.show
 	end
 
