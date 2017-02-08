65976e26c151ae3f45e0959aa15fb6e5c0291118
mariusmaxetselina <maxetselina@gmail.com>
2017-02-08 22:07:29 +0100

    modification de CellView et GridPanel

--- vue/CellView.rb
+++ vue/CellView.rb
@@ -1,17 +1,23 @@
 require 'gtk3'
 require_relative '../class/cell_class.rb'
 
-class CellView
-	
+class CellView < Gtk::Button
+##choix à faire : button, frame or widget ?
 	attr_accessor :button
 	attr_accessor :cell
 
 	def initialize(cell)
+		
 		@cell=cell
 		if(@cell.value==0)
-			@button=Gtk::Button.new(:label=>" ")
+			#@button=Gtk::Button.new(:label=>" ")
+			super(" ")
+			
 		else
-			@button=Gtk::Button.new(:label=>@cell.value.to_s)
+			#@button=Gtk::Button.new(:label=>@cell.value.to_s)
+			super(:label=>@cell.value.to_s)
 		end
+		#set_border_width(0)
+		#add(@button)
 	end
 end
