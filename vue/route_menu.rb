c5483684f3e5d6e7bf4423ad0b285cb9db7a4f4a
mariusmaxetselina <maxetselina@gmail.com>
2017-02-27 15:14:24 +0100

    modif des noms de fichiers et des classes attribuées

---
+++ vue/route_menu.rb
@@ -0,0 +1,52 @@
+require 'gtk3'
+require_relative '../vue/route_option.rb'
+require_relative '../vue/route_libre_choix.rb'
+
+class Menu < Gtk::Window
+
+	attr_accessor :event1
+
+	def initialize()
+		super()
+		set_title "Bienvenue Sudoku"
+		set_default_size 300, 300
+		set_resizable false
+
+		signal_connect 'destroy'  do
+			Gtk.main_quit
+		end
+		init_ui
+	end
+
+	def init_ui
+
+		@event1 = Gtk::VBox.new(:vertical=>true,:spacing=>5)
+		label_title = Gtk::Label.new "Sudoku", :use_underline => true
+		tutoButton = Gtk::Button.new :label=>"Tuto", :use_underline => true
+		arcadeButton = Gtk::Button.new :label=>"Arcade", :use_underline => true
+		freeModeButton = Gtk::Button.new :label=>"Libre", :use_underline => true
+		optionButton = Gtk::Button.new :label=>"Options", :use_underline => true
+		@event1.add(label_title)
+		@event1.add(tutoButton)
+		@event1.add(arcadeButton)
+		@event1.add(freeModeButton)
+		@event1.add(optionButton)
+
+		optionButton.signal_connect("clicked"){
+		remove(@event1)
+		add(Option.new self)
+		}
+
+		freeModeButton.signal_connect("clicked"){
+		remove(@event1)
+		add(FreeModeChoice.new self)
+		}
+		add @event1
+
+		show_all
+	end
+end
+
+#Gtk.init
+Menu.new()
+Gtk.main
