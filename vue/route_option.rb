ccf8c38b0c593d62bc0e8167d7e568ced9c53b78
mariusmaxetselina <maxetselina@gmail.com>
2017-03-15 14:14:31 +0100

    centrer les fenetres

--- vue/route_option.rb
+++ vue/route_option.rb
@@ -6,7 +6,7 @@ class Option < Gtk::Frame
 	def initialize(window)
 		super()
 		@window=window
-		
+		@window.set_window_position Gtk::Window::Position::CENTER
 		event1 = Gtk::Box.new(:vertical,5)
 		label_title = Gtk::Label.new "Options", :use_underline => true
 		changeColorButton = Gtk::Button.new :label=>"Changer les couleurs", :use_underline => true
