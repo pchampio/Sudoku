74224d3673f2b8bb5b3bf2aef5d18f95c20d0203
Dezere Florian <florian.Dezere@gmail.com>
2017-03-23 15:32:40 +0100

    presque fini

--- vue/route_libre_win.rb
+++ vue/route_libre_win.rb
@@ -7,18 +7,14 @@ class FreeModeWin < Gtk::Frame
 		@window = window
 		@window.set_window_position Gtk::WindowPosition::CENTER
 		@window.set_title "Sudoku (Victoire)"
-		imageButton = Gtk::Button.new :label=>"Image"
+
+		victoire = Gtk::Image.new_from_file("../ressources/victoire.jpg")
+
+		imageButton = Gtk::Button.new :label=>"Image", :image=>victoire
 	
-		victoire = Gtk::Image.new(:file => "../ressources/victoire.jpg")
-		imageButton.set_image(victoire)
 		@event = Gtk::Box.new(:vertical,2)
 		@event.add(imageButton)
 		self.add(victoire)
-		#@myViewport = Viewport.new(0, 0, 640, 480)
-		#@mySprite = Sprite.new(@myViewport)
-		#@myBitmap = RPG::Cache.picture("../ressources/victoire.png")
-		#@mySprite.bitmap = @myBitmap
-		#@window.add(@mySprite)
 		show_all
 	end
 
