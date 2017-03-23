99d0683cc6ad0b5a96e8fe525e9fbb5cb5cee916
Dezere Florian <florian.Dezere@gmail.com>
2017-03-23 16:14:51 +0100

    image de victoire, ok !

--- vue/route_libre_win.rb
+++ vue/route_libre_win.rb
@@ -8,14 +8,18 @@ class FreeModeWin < Gtk::Frame
 		@window.set_window_position Gtk::WindowPosition::CENTER
 		@window.set_title "Sudoku (Victoire)"
 
-		victoire = Gtk::Image.new_from_file("../ressources/victoire.jpg")
-
-		imageButton = Gtk::Button.new :label=>"Image", :image=>victoire
-	
-		@event = Gtk::Box.new(:vertical,2)
-		@event.add(imageButton)
-		self.add(victoire)
+		imgVictoire = Gtk::Image.new :file => "./ressources/victoire.png"
+	    boxVictoire = Gtk::Box.new(:horizontal,1)
+	    boxVictoire.add(imgVictoire)
+	    self.add(boxVictoire)
 		show_all
 	end
 
 end
+
+
+
+#Pour les boutons retours, il faut qu'ils apparaissent dans la barre en haut
+#la pause doit faire un save de la grille, temps (==> serial) affiche ecran noir, puis revient sur la grille deserialiser
+#ajout du temps sur la victoire
+#label des boutons a remplacer par des box / grilles
