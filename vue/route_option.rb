f09edb9e95c2fa003fb3a6dd694f34550618c021
Dezere Florian <florian.dezere@gmail.com>
2017-03-30 08:21:10 +0000

    ajout color picker surlignage

--- vue/route_option.rb
+++ vue/route_option.rb
@@ -1,3 +1,14 @@
+#!/usr/bin/env ruby
+
+# encoding: UTF-8
+
+##
+# Author:: DEZERE Florian
+# License:: MIT Licence
+#
+# https://github.com/Drakirus/Sudoku
+#
+
 require 'gtk3'
 require_relative './serialisable.rb'
 
@@ -11,55 +22,64 @@ class Option < Gtk::Frame
 		vBox = Gtk::Box.new(:vertical,10)
 		vBox.set_homogeneous("r")
 
-    scpicker = Gtk::ColorButton.new(Serialisable.getSelectColor)
+		scpicker = Gtk::ColorButton.new(Serialisable.getSelectColor)
 		bgpicker = Gtk::ColorButton.new(Serialisable.getBackgroundColor)
 		cpicker = Gtk::ColorButton.new(Serialisable.getChiffreColor)
+		surlignepicker = Gtk::ColorButton.new(Serialisable.getSurligneColor)
 
-    bgHBox = Gtk::Box.new(:horizontal,3)
+		bgHBox = Gtk::Box.new(:horizontal,3)
 		label_background = Gtk::Label.new "Couleur du fond de la grille :", :use_underline => true
 		bgpicker.signal_connect("color-set"){
-   		   Serialisable.setBackgroundColor(bgpicker.color)
+			Serialisable.setBackgroundColor(bgpicker.color)
 		}
 
-    bgHBox.set_homogeneous("r")
-    bgHBox.add label_background
-    bgHBox.add bgpicker
+		bgHBox.set_homogeneous("r")
+		bgHBox.add label_background
+		bgHBox.add bgpicker
 
 
-    scHBox = Gtk::Box.new(:horizontal,3)
-    label_selected_cell = Gtk::Label.new "Couleur de la case selectionnée :", :use_underline => true
+		scHBox = Gtk::Box.new(:horizontal,3)
+		label_selected_cell = Gtk::Label.new "Couleur de la case selectionnée :", :use_underline => true
 		scpicker.signal_connect("color-set"){
 			Serialisable.setSelectColor(scpicker.color)
 		}
 
-    scHBox.set_homogeneous("r")
-    scHBox.add label_selected_cell
-    scHBox.add scpicker
+		scHBox.set_homogeneous("r")
+		scHBox.add label_selected_cell
+		scHBox.add scpicker
 
 
-    cchiffHbox = Gtk::Box.new(:horizontal,3)
-    label_num_color = Gtk::Label.new "Couleur des chiffres :", :use_underline => true
+		cchiffHbox = Gtk::Box.new(:horizontal,3)
+		label_num_color = Gtk::Label.new "Couleur des chiffres :", :use_underline => true
 		cpicker.signal_connect("color-set"){
 			Serialisable.setChiffreColor(cpicker.color)
 		}
-    cchiffHbox.set_homogeneous("r")
-    cchiffHbox.add label_num_color
-    cchiffHbox.add cpicker
+		cchiffHbox.set_homogeneous("r")
+		cchiffHbox.add label_num_color
+		cchiffHbox.add cpicker
+
+		cSurlignHbox = Gtk::Box.new(:horizontal, 3)
+		label_surligne_color = Gtk::Label.new "Couleur de surlignage", :use_underline => true
+		surlignepicker.signal_connect("color-set"){
+			Serialisable.setSurligneColor(surlignepicker.color)
+		}
+		cSurlignHbox.set_homogeneous("r")
+		cSurlignHbox.add label_surligne_color
+		cSurlignHbox.add surlignepicker
 
 		menuButton=Gtk::Button.new(:label=>"Retour")
 		menuButton.signal_connect("clicked"){
-      		Serialisable.serialized
+			Serialisable.serialized
 			@window.remove self
 			@window.add @window.main_menu
 		}
 
-    vBox.add bgHBox
-    vBox.add scHBox
-    vBox.add cchiffHbox
+		vBox.add bgHBox
+		vBox.add scHBox
+		vBox.add cchiffHbox
 		vBox.add menuButton
 		self.add vBox
 
 		show_all
 	end
-
 end
--- vue/serialisable.rb
+++ vue/serialisable.rb
@@ -2,12 +2,13 @@ require 'gtk3'
 
 class Serialisable
 
-  attr_reader :selectColor, :backgroundColor, :chiffreColor
+  attr_reader :selectColor, :backgroundColor, :chiffreColor, :surligneColor
 
 	def initialize()
 	    @backgroundColor =  @@backgroundColor.to_s
 	    @selectColor     =  @@selectColor.to_s
 	    @chiffreColor	 =	@@chiffreColor.to_s
+	    @surligneColor 	 = 	@@surligneColor.to_s
 	end
 
 	def self.getBackgroundColor()
@@ -34,6 +35,14 @@ class Serialisable
 		@@chiffreColor = chiffreColor
 	end
 
+	def self.getSurligneColor()
+		return @@surligneColor
+	end
+
+	def self.setSurligneColor(surligneColor)
+		@@surligneColor = surligneColor
+	end
+
 	def self.serialized
     obj = Serialisable.new
 		File.open("save_color.yaml", "w+") do |f|
@@ -46,6 +55,7 @@ class Serialisable
    	self.setBackgroundColor(Gdk::Color.parse(obj.backgroundColor))
     self.setSelectColor(Gdk::Color.parse(obj.selectColor))
 	self.setChiffreColor(Gdk::Color.parse(obj.chiffreColor))
+	self.setSurligneColor(Gdk::Color.parse(obj.selectColor))
 	end
 
 end
