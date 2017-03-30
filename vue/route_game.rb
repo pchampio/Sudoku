59afe0a0c85a0f940ec9f808aba6a8c80fef859f
Drakirus <pierrescalade@gmail.com>
2017-03-30 11:23:37 +0200

    game overlay

--- vue/route_game.rb
+++ vue/route_game.rb
@@ -4,8 +4,9 @@ require 'thread'
 
 
 Dir[File.dirname(__FILE__) + '/*.rb'].each {|file| require file }
+require_relative "../class/generator_class.rb"
 
-class Game < Gtk::Frame
+class Game < Gtk::Overlay
 
   def initialize(window,board)
     super()
@@ -15,10 +16,66 @@ class Game < Gtk::Frame
 
     hBox = Gtk::Box.new(:horizontal,2)
     boardComponent = BoardComponent.create board
-    inGameMedu = InGameMenu.create(self, boardComponent)
+    inGameMedu = InGameMenu.create(boardComponent)
 
     hBox.add(boardComponent)
     hBox.add(inGameMedu)
+
+    @backgroundColor = window.style_context.get_background_color "NORMAL"
+
+    init_overlay
+    showOverlay
+
+
+    vbox = Gtk::Box.new(:vertical, 10)
+    vbox.halign = :center
+    vbox.valign = :center
+    button = Gtk::Button.new(:label =>"coucou")
+    button.name = "buttontest"
+    vbox.add button
+    vbox.set_margin_top 10
+    vbox.set_margin_bottom 10
+    button.name = "testbtn"
+    vbox.name = "testsdf"
+
+    apply_css_color_button(button, "background", Serialisable.getBackgroundColor)
+    button.signal_connect "clicked" do
+      hideOverlay
+    end
+
+    addToOverlay vbox
+
+    self.set_overlay_pass_through(@frame, false)
     self.add(hBox)
   end
+
+  def init_overlay
+    @frame = Gtk::Frame.new
+    @frame.name = "wind"
+
+
+    css=<<-EOT
+     #wind {
+      background-color: #{@backgroundColor};
+      opacity: 0.9;
+     }
+    EOT
+    css_provider = Gtk::CssProvider.new
+    css_provider.load :data=>css
+    apply_css(@frame, css_provider)
+
+  end
+
+  def hideOverlay
+    self.remove(@frame)
+  end
+
+  def showOverlay
+    self.add_overlay(@frame)
+  end
+
+  def addToOverlay box
+    @frame.add box
+  end
+
 end
