f75713d2d8fb45dd015ebfb8cdca5beff4929b39
mariusmaxetselina <maxetselina@gmail.com>
2017-04-05 16:06:07 +0200

    overlay accueil

--- vue/overlay_accueil.rb
+++ vue/overlay_accueil.rb
@@ -9,17 +9,26 @@ class Accueil < Gtk::Frame
 		super()
 		username = Gtk::Label.new "<span weight='ultrabold' font='16'> Bienvenue "+SaveUser.getUsername+" !</span>"
 		username.use_markup = true
+
 		vbox = Gtk::Box.new :vertical, 5
-		vbox.pack_start username, :expand=>false, :fill=>false, :padding=>15
 
-		vbox.pack_start Gtk::Label.new('Vous avez : '+ SaveUser.getNbEtoile.to_s+" ☆"),:expand=>false, :fill=>false, :padding=>15
+		hbox0= Gtk::Box.new :horizontal, 1
+		hbox0.pack_start username, :expand=>false, :fill=>false, :padding=>15
+		vbox.pack_start hbox0,:expand=>false, :fill=>false, :padding=>15
 
-		vbox.pack_start Gtk::Label.new 'Vous avez joué '+Timer.getTimeAccueil(SaveUser.getTime),:expand=>false, :fill=>false, :padding=>15
+		hbox1=Gtk::Box.new :horizontal,1
+		hbox1.pack_start Gtk::Label.new('Vous avez : '+ SaveUser.getNbEtoile.to_s+" ☆"),:expand=>false, :fill=>false, :padding=>15
+		vbox.pack_start hbox1,:expand=>false, :fill=>false, :padding=>15
+
+		hbox2= Gtk::Box.new :horizontal,1
+		hbox2.pack_start Gtk::Label.new 'Vous avez joué '+Timer.getTimeAccueil(SaveUser.getTime)+".",:expand=>false, :fill=>false, :padding=>15
+		vbox.pack_start hbox2,:expand=>false, :fill=>false, :padding=>15
+
 		@retour = Gtk::Button.new(:label=>"continuer")
 		vbox.pack_start @retour, :expand=>false, :fill=>false, :padding=>0
 
-		vbox.halign = :CENTER
-		vbox.valign = :CENTER
+		#vbox.halign = :CENTER
+		#vbox.valign = :CENTER
 		self.add(vbox)
 	end
 
